0fd215018be994378c2d004c6c0a76444186df05
ritakool <jokernguyen1994@gmail.com>
2023-03-25 02:50:16 +0700

    updateDrawheart

--- src/Main.java
+++ src/Main.java
@@ -10,6 +10,7 @@ public class Main {
             System.out.println("1. Print the rectangle");
             System.out.println("2. Print the square triangle");
             System.out.println("3. Print isosceles triangle");
+            System.out.println("5. Print heart");
             System.out.println("4. Exit");
             System.out.println("Enter your choice: ");
             choice = sc.nextByte();
@@ -80,6 +81,39 @@ public class Main {
                     }
                     System.out.print("\n");
                     break;
+                case 5:
+                    System.out.println("Print heart");
+                    for (int i = 1; i <= 6; i += 2) {
+                        for (int j = 1; j <= 6 - i; j += 2) {
+                            System.out.print("  ");
+                        }
+                        for (int j = 1; j <= i; j++) {
+                            System.out.print("* ");
+                        }
+                        for (int j = 1; j <= 6 - i; j++) {
+                            System.out.print("  ");
+                        }
+                        for (int j = 1; j <= i; j++) {
+                            System.out.print("* ");
+                        }
+                        System.out.println();
+                    }
+                    for (byte i = 1; i <= 6; i++) {
+
+                        for (byte j = 1; j <= i; j++) {
+                            System.out.print("  ");
+                        }
+                        for (byte j = i; j < 6; j++) {
+                            System.out.print("* ");
+                        }
+                        for (byte j = i; j <= 6; j++) {
+                            System.out.print("* ");
+                        }
+                        System.out.print("\n");
+                    }
+                    break;
+                case 4:
+                    System.exit(4);
             }
         }
     }
